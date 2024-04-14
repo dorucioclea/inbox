452d4b0d76d063e061440be748a60e1b5f1777a9
McPizza <gh@l1.io>
2024-04-14 13:58:31 +0100

    fix(web): redirect if 2FA error

--- apps/web-app/pages/redirect.vue
+++ apps/web-app/pages/redirect.vue
@@ -1,44 +1,57 @@
 <script setup lang="ts">
-  import { navigateTo, useNuxtApp, onMounted, useCookie } from '#imports';
+  import { navigateTo, useNuxtApp, onMounted, useCookie, ref } from '#imports';
 
   const { $trpc } = useNuxtApp();
 
+  const data = ref<any>(null);
+
   onMounted(async () => {
-    const { defaultOrgShortcode, twoFactorEnabledCorrectly, username } =
-      await $trpc.account.defaults.redirectionData.query({});
+    const {
+      defaultOrgShortcode,
+      twoFactorEnabledCorrectly,
+      userHasOrgMembership,
+      username
+    } = await $trpc.account.defaults.redirectionData.query({});
 
     const usernameCookie = useCookie('un-join-username', {
       maxAge: 3600
     });
 
-    if (
-      process.client && // running in client
-      !usernameCookie.value && // doesn't have a username cookie
-      username && // has username set
-      Boolean(defaultOrgShortcode) && // has a default org
-      !twoFactorEnabledCorrectly // 2fa is not enabled correctly
-    ) {
-      usernameCookie.value = username;
+    data.value = twoFactorEnabledCorrectly;
+
+    if (!userHasOrgMembership) {
+      if (!twoFactorEnabledCorrectly) {
+        if (
+          process.client && // running in client
+          !usernameCookie.value && // doesn't have a username cookie
+          username && // has username set
+          Boolean(defaultOrgShortcode) && // has a default org
+          !twoFactorEnabledCorrectly // 2fa is not enabled correctly
+        ) {
+          usernameCookie.value = username;
+        }
+        return navigateTo(`/join/2fa`);
+      }
+      return navigateTo(`/join/org`);
     }
 
     if (!twoFactorEnabledCorrectly) {
-      return navigateTo(`/join/2fa`);
-    }
-
-    if (!defaultOrgShortcode) {
-      return navigateTo(`/join/org`);
+      return navigateTo(
+        `/${defaultOrgShortcode}/settings/user/security?2fa=error`
+      );
     }
 
     // We need to redirect to the index page of [orgShortcode] due to this nuxt issue https://github.com/nuxt/nuxt/issues/25214
     // the index page will reload nuxt, then redirect to the convos view - we should not directly navigate to convos page!!!
-    // setTimeout(() => {
-    navigateTo(`/${defaultOrgShortcode}`);
-    // }, 500);
+    setTimeout(() => {
+      navigateTo(`/${defaultOrgShortcode}`);
+    }, 500);
   });
 </script>
 
 <template>
   <div class="flex h-full w-full items-center justify-center">
     <span class="font-display text-3xl">Redirecting...</span>
+    {{ data }}
   </div>
 </template>
